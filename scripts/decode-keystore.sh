c8f1a278ef93615b4786e9fb18a0198969ecfbfa
David Reband <david.reband@webanizer.de>
2022-05-23 14:10:35 +0200

    add JSON_KEY_FILE and RELEASE_KEYSTORE_PROPERTIES | decode-keystore.sh

--- scripts/decode-keystore.sh
+++ scripts/decode-keystore.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
- echo $ENV_KEYSTORE_BASE64  | base64 --decode > /home/DoiWallet/android/app/upload-keystore.jks 
+ echo $ENV_KEYSTORE_BASE64  | base64 --decode > /home/DoiWallet/android/app/upload-keystore.jks
+
+ echo $ENV_JSON_KEY_FILE  | base64 --decode > /home/DoiWallet/android/keystores/pc-api-7544041306212151668-842-081c5b393ff8.json 
+  
+ echo $ENV_RELEASE_KEYSTORE_PROPERTIES | base64 --decode > /home/DoiWallet/android/keystores/keystore_files/release.keystore.properties
+
+
