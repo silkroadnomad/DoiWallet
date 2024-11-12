faf8574fd25740a869be8cac67bfb4305d95c16b
David Reband <david.reband@webanizer.de>
2024-11-12 08:27:46 +0100

    library replacement @doichain/doichainjs-lib

--- tests/integration/hd-legacy-breadwallet.test.ts
+++ tests/integration/hd-legacy-breadwallet.test.ts
@@ -1,5 +1,5 @@
 import assert from 'assert';
-import * as bitcoin from 'bitcoinjs-lib';
+import * as bitcoin from '@doichain/doichainjs-lib';
 
 import * as BlueElectrum from '../../blue_modules/BlueElectrum';
 import { HDLegacyBreadwalletWallet } from '../../class';
