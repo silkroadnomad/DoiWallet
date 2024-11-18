1044b85d81b33b205b46b117a7446348f91f7504
David Reband <david.reband@webanizer.de>
2024-11-18 16:00:03 +0100

    optimized the skipped tests in the file segwit-p2sh-wallet.test.js

--- tests/unit/segwit-p2sh-wallet.test.js
+++ tests/unit/segwit-p2sh-wallet.test.js
@@ -2,13 +2,14 @@ import assert from 'assert';
 import * as bitcoin from "@doichain/doichainjs-lib";
 
 import { SegwitP2SHWallet } from '../../class';
+import { DOICHAIN } from '../../blue_modules/network';
 
 describe('Segwit P2SH wallet', () => {
-  it.skip('can create transaction', async () => {
+  it('can create transaction', async () => {
     const wallet = new SegwitP2SHWallet();
-    wallet.setSecret('Ky1vhqYGCiCbPd8nmbUeGfwLdXB1h5aGwxHwpXrzYRfY5cTZPDo4');
-    assert.strictEqual(wallet.getAddress(), '3CKN8HTCews4rYJYsyub5hjAVm5g5VFdQJ');
-    assert.deepStrictEqual(wallet.getAllExternalAddresses(), ['3CKN8HTCews4rYJYsyub5hjAVm5g5VFdQJ']);
+    wallet.setSecret('TkpCRCZ7tPwcHWjVHGrFdeYYL5sbQ5FzU4JmzUx3BDDMiKcw2yUG');    
+    assert.strictEqual(wallet.getAddress(), '6EWSDEnXSLHAeNwrtxnZpYWDicbzKjXdGz');
+    assert.deepStrictEqual(wallet.getAllExternalAddresses(), ['6EWSDEnXSLHAeNwrtxnZpYWDicbzKjXdGz']);
     assert.strictEqual(await wallet.getChangeAddressAsync(), wallet.getAddress());
     assert.strictEqual(await wallet.getAddressAsync(), wallet.getAddress());
 
@@ -20,33 +21,33 @@ describe('Segwit P2SH wallet', () => {
       },
     ];
 
-    let txNew = wallet.createTransaction(utxos, [{ value: 90000, address: '1GX36PGBUrF8XahZEGQqHqnJGW2vCZteoB' }], 1, wallet.getAddress());
+    let txNew = wallet.createTransaction(utxos, [{ value: 90000, address: '6EWSDEnXSLHAeNwrtxnZpYWDicbzKjXdGz' }], 1, wallet.getAddress());
     let tx = bitcoin.Transaction.fromHex(txNew.tx.toHex());
     const satPerVbyte = txNew.fee / tx.virtualSize();
     assert.strictEqual(Math.round(satPerVbyte), 1);
     assert.strictEqual(
       txNew.tx.toHex(),
-      '020000000001010c86eb9013616e38b4752e56e5683e864cb34fcd7fe790bdc006b60c08446ba50000000017160014139dc70d73097f9d775f8a3280ba3e3435515641ffffffff02905f0100000000001976a914aa381cd428a4e91327fd4434aa0a08ff131f1a5a88aca73303000000000017a914749118baa93fb4b88c28909c8bf0a8202a0484f4870248304502210080545d30e3d30dff272ab11c91fd6150170b603239b48c3d56a3fa66bf240085022003762404e1b45975adc89f61ec1569fa19d6d4a8d405e060897754c489ebeade012103a5de146762f84055db3202c1316cd9008f16047f4f408c1482fdb108217eda0800000000',
+      '020000000001010c86eb9013616e38b4752e56e5683e864cb34fcd7fe790bdc006b60c08446ba500000000171600142359b9bd0cb14ce680e11362836e2c5a56bc96a7ffffffff02905f01000000000017a914013f386fb8e9dd4063a96ecf7f703d54bfbd108587a73303000000000017a914013f386fb8e9dd4063a96ecf7f703d54bfbd1085870247304402205dc8e8dd2faefe249d52b26a5109df306826b29778bde86ef5f1cee29cafb41402204c46e5d3b46549edb83a00520669d708e3fd97fa195cfa04c49b7bdb72354f01012102b58e16feba6b0ce28842b1a0aba18c85a5e945db84d0f6a3ba30b38edccfe97300000000',
     );
     assert.strictEqual(tx.ins.length, 1);
     assert.strictEqual(tx.outs.length, 2);
-    assert.strictEqual('1GX36PGBUrF8XahZEGQqHqnJGW2vCZteoB', bitcoin.address.fromOutputScript(tx.outs[0].script)); // to address
-    assert.strictEqual(bitcoin.address.fromOutputScript(tx.outs[1].script), wallet.getAddress()); // change address
+    assert.strictEqual('6EWSDEnXSLHAeNwrtxnZpYWDicbzKjXdGz', bitcoin.address.fromOutputScript(tx.outs[0].script, DOICHAIN)); // to address
+    assert.strictEqual(bitcoin.address.fromOutputScript(tx.outs[1].script, DOICHAIN), wallet.getAddress()); // change address
 
     // sendMax
-    txNew = wallet.createTransaction(utxos, [{ address: '1GX36PGBUrF8XahZEGQqHqnJGW2vCZteoB' }], 1, wallet.getAddress());
+    txNew = wallet.createTransaction(utxos, [{ address: '6EWSDEnXSLHAeNwrtxnZpYWDicbzKjXdGz' }], 1, wallet.getAddress());
     tx = bitcoin.Transaction.fromHex(txNew.tx.toHex());
     assert.strictEqual(tx.ins.length, 1);
     assert.strictEqual(tx.outs.length, 1);
-    assert.strictEqual('1GX36PGBUrF8XahZEGQqHqnJGW2vCZteoB', bitcoin.address.fromOutputScript(tx.outs[0].script)); // to address
+    assert.strictEqual('6EWSDEnXSLHAeNwrtxnZpYWDicbzKjXdGz', bitcoin.address.fromOutputScript(tx.outs[0].script, DOICHAIN)); // to address
   });
 
-  it.skip('can sign and verify messages', async () => {
+  it('can sign and verify messages', async () => {
     const l = new SegwitP2SHWallet();
-    l.setSecret('L4rK1yDtCWekvXuE6oXD9jCYfFNV2cWRpVuPLBcCU2z8TrisoyY1'); // from bitcoinjs-message examples
+    l.setSecret('TkpCRCZ7tPwcHWjVHGrFdeYYL5sbQ5FzU4JmzUx3BDDMiKcw2yUG'); // from bitcoinjs-message examples
 
     const signature = l.signMessage('This is an example of a signed message.', l.getAddress());
-    assert.strictEqual(signature, 'I9L5yLFjti0QTHhPyFrZCT1V/MMnBtXKmoiKDZ78NDBjERki6ZTQZdSMCtkgoNmp17By9ItJr8o7ChX0XxY91nk=');
+    assert.strictEqual(signature, 'I4X/eco2cjmFb4L2+gts0RpUlMxaOQ9WJc40TSL6wQ5/UG9qbxhdnWqIoPqtcqmqx0j34ehWnoL6vUg3MvpqPuU=');
     assert.strictEqual(l.verifyMessage('This is an example of a signed message.', l.getAddress(), signature), true);
   });
 });
